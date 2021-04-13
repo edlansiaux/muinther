2e78feef82562625428367402da365a151080204
Edouard Lansiaux <edouard.lansiaux@orange.fr>
2021-04-13 08:18:25 +0200

    Update

--- R/loop.R
+++ R/loop.R
@@ -1,19 +1,20 @@
 #' @title Loop to computate all entropy metrics outputs
 #' @description Table of mutual information theory coefficients for variables association study. Computated outputs gather: X variable name, Y variable name, X information entropy, Y information entropy, Computed marginal EPMF of X, Computed marginal EPMF of Y, Chi2, Chi2 p-value, Information entropy of X, Information entropy of Y, Joint information entropy of X and Y, Conditional information entropy of Y given X, Conditional information entropy of X given Y, Mutual information of X and Y, Normalized mutual information of X and Y.
 #' @references Shannon, C. A mathematical theory of communication. Bell Labs Tech. J. 27, 379–423, DOI: 10.1002/j.1538-7305.1948.tb01338.x (1948).
-#' @param fn is the path to source data csv file
+#' @param input is the source data frame
 #' @param m is the first studied X variable column number
 #' @param n is the studied variables number
-#' @examples loop('I:/muinther/R/muinther/data/docs_phenotype_file_1.csv',1,8)
+#' @examples loop('docs_phenotype_file_1',1,8)
 #'
 #' @import reticulate
 #' @export
 
-loop <- function(fn,m,n){
+loop <- function(input,m,n){
+  write.csv(input,'input.csv')
   os <- reticulate::import("os")
   reticulate::source_python('I:/muinther/R/muinther/R/loop.py')
-  loopy(fn,m,n)
-  fn1 <- as.data.frame(readr::read_csv(fn))
+  loopy('input.csv',m,n)
+  fn1 <- as.data.frame(readr::read_csv('input.csv'))
   fn1 <- fn1[, (m+1):(m+n)]
   var_1 <- colnames(fn1)
   entropy_outputs <- as.data.frame(readr::read_csv('entropy_outputs.csv'))
